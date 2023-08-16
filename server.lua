967ecad52813a7e48f4fb87595c67a5b3a093183
ItsKuf <104674648+ItsKuf@users.noreply.github.com>
2023-08-16 15:00:14 +0100

    Update server.lua

--- server.lua
+++ server.lua
@@ -1,2 +1,8 @@
 QBCore = nil
 TriggerEvent('QBCore:GetObject', function(obj) QBCore = obj end)
+
+--If you are standalone please use this below and chnage 'core' with the name of yours if it's wrong.
+local core = nil
+TriggerEvent('core:GetObject', function(obj)
+    core = obj
+end)
