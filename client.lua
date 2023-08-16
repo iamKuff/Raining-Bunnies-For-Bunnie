e1f755ec8736b10d5f7bc465db7d2a1d7bf908ea
ItsKuf <104674648+ItsKuf@users.noreply.github.com>
2023-08-16 17:45:42 +0100

    Update client.lua
    The new version corrects the spawn point's Z-coordinate using a separate variable and organizes the code more clearly, while the old version directly adjusts the Z-coordinate and lacks optimal organization.

--- client.lua
+++ client.lua
@@ -6,8 +6,13 @@ Citizen.CreateThread(function()
         local playerCoords = GetEntityCoords(playerPed)
         
         local spawnCoords = GetOffsetFromEntityInWorldCoords(playerPed, 0.0, math.random(-50, 50) + 0.0, 0.0)
-        local ground, spawnZ = GetGroundZFor_3dCoord(spawnCoords.x, spawnCoords.y, spawnCoords.z, 0)
-        spawnCoords.z = spawnZ
+        
+        local spawnX = spawnCoords["x"] 
+        local spawnY = spawnCoords["y"]  
+        local spawnZ = spawnCoords["z"]  
+        
+        local ground, newSpawnZ = GetGroundZFor_3dCoord(spawnX, spawnY, spawnZ, 0)
+        spawnCoords = vector3(spawnX, spawnY, newSpawnZ)
         
         if IsModelValid(Config.BunnyModel) then
             RequestModel(Config.BunnyModel)
