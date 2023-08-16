54e1b790406934aac98db31e0f079933baf3470c
ItsKuf <104674648+ItsKuf@users.noreply.github.com>
2023-08-16 17:46:52 +0100

    Update config.lua
    Removed Personal message to Bunnie lol

--- config.lua
+++ config.lua
@@ -1,3 +1,2 @@
 Config = {}
 Config.BunnyModel = "a_c_rabbit_01"  -- Do not change! THIS IS IMPORTANT BUNNIE!
-Config.PersonalMessage = "Hey Bunnie my love, this one's for you! Enjoy the bunny rain!"
